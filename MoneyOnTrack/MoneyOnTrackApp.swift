4109aa978feab748005c9b4eee44ea3ddee8406c
Arda Berktin <arberktin@gmail.com>
2024-01-03 03:50:24 -0800

    Initial Commit

---
+++ MoneyOnTrack/MoneyOnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyOnTrackApp.swift
+//  MoneyOnTrack
+//
+//  Created by Arda Berktin on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct MoneyOnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
