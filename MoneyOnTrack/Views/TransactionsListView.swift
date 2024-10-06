490f9e53a1baa52f656cec2dd2ea99c0c4589c42
Arda Berktin <arberktin@gmail.com>
2024-10-06 13:46:41 -0700

    Added TransactionsListView

---
+++ MoneyOnTrack/Views/TransactionsListView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionsListView.swift
+//  MoneyOnTrack
+//
+//  Created by Arda Berktin on 2024-10-06.
+//
+
+import SwiftUI
+
+struct TransactionsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionsListView()
+}
